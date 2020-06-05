80ff63ea869ebd0ad2d52bae205c8548ab5640e7
Facundotorrez <ftorrez@udesa.edu.ar>
2020-06-05 18:06:55 -0300

    cambios en la home.js y en el home.html

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 window.addEventListener ("load", function()  {
-    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27")
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/")
     .then(
         function (respuesta){
             return respuesta.json();
@@ -7,18 +7,24 @@ window.addEventListener ("load", function()  {
     )
     .then(
         function(information) {
+           let track = document.querySelector ("li .track-item") 
+
             let trackList = information.tracks.data;
             for (let i = 0; i < trackList.length; i++) {
                 let trackId = trackList[i].id;
                 let trackTitle = trackList[i].title_short;
                 let trackArtist = trackList[i].artist.name;
                 let trackArtistId = trackList[i].artist.id;
-                let trackItem = `
-                <li class='track-item'>
-                    <a class='tNomb' href='track.html?trackID=` + trackId +`'>` + trackTitle + `</a>
-                    <span> by <a class='aNomb' href='artist.html?artistID=` + trackArtistId + `'>` + trackArtist +  `</a></span>
-                </li>
-                `;
+                let trackImage = trackList[i].cover;
+                let trackItem = `      <div class="grid-item">
+                <a href="top-tracks.html"> 
+                  <img class="imageSize" src= `+trackImage+`>
+                </a>
+                <br>
+                <a class="item" href="top-tracks.html">`+trackTitle+`</a>
+              </div>`
+        
+            
                 document.querySelector(".trackList").innerHTML += trackItem ;
             }
         }
