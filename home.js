f2ec2cfc6cf657fbf594647836e05416ae5bce5c
bauti-saud <you@example.com>
2020-06-12 15:22:48 -0300

    nada

--- home.js
+++ home.js
@@ -8,27 +8,29 @@ window.addEventListener ("load", function()  {
     .then(
         function(information) {
            let track = document.querySelector ("li .track-item") 
-
             let trackList = information.tracks.data;
             for (let i = 0; i < trackList.length; i++) {
+                console.log(trackList);
                 let trackId = trackList[i].id;
                 let trackTitle = trackList[i].title_short;
                 let trackArtist = trackList[i].artist.name;
                 let trackArtistId = trackList[i].artist.id;
-                let trackImage = trackList[i].cover;
-                let trackItem = `      <div class="grid-item">
-                <a href="top-tracks.html"> 
-                  <img class="imageSize" src= `+trackImage+`>
-                </a>
-                <br>
-                <a class="item" href="top-tracks.html">`+trackTitle+`</a>
-              </div>`
-        
-            
-                document.querySelector(".trackList").innerHTML += trackItem ;
+                let trackImage = trackList[i].album.cover;
+                let trackItem = `
+                <li class="track-item">
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <a href="artist.html">  <img src="` + trackImage + `" alt=""> </a>
+                    </div>
+                    <div class="uk-card-body">
+                        <a href="artist.html" > <h3 class="uk-card-title">` + trackTitle + `<h3></a>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+                </li>
+                `
+                document.querySelector(".trackList").innerHTML += trackItem;
             }
         }
     )
 })
-
-// Sacar los corchetes
