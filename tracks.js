a878e5a91ca2989253162d3053c79371dc493c3f
Timoteo Masic <timoteomasic@Timos-MacBook-Pro.local>
2020-06-17 23:32:26 -0300

    Cambios

---
+++ tracks.js
@@ -0,0 +1,32 @@
+window.addEventListener ("load", function()  {  
+    
+    let queryString = location.search;
+    let datos = new URLSearchParams (queryString);
+    let idTrack = datos.get ('id');
+    console.log(idTrack); 
+    
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/" + idTrack)
+    .then(
+        function (respuesta){
+            return respuesta.json();
+        
+        }  
+    )
+    .then(function(datos){
+    console.log(datos)
+    let mainTrack = document.querySelector(".mainTrack")
+    mainTrack.innerHTML = `
+        <div class="grid-item"> 
+            <img class="imageSize" src=`+ datos.album.cover_medium +`>
+        </div>
+
+        <article class="trackInfo">
+            <div class="artistDetails">
+              <h1>`+ datos.artist.name +`</h1>
+              <h3>`+ datos.album.title +`</p>
+              <button class="boton"><i class="far fa-heart"></i></button>
+            </div>
+        </article>
+    `
+    });
+})
